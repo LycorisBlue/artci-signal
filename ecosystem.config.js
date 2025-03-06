3089ae4282f95561f7204455665891248ad282c8
Medi G Fulgence <lycorisblue99@gmail.com>
2025-03-06 23:48:08 +0000

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,27 @@
 module.exports = {
     apps: [
         {
-            name: 'artci-signal', // Nom de ton application
-            script: 'node_modules/next/dist/bin/next', // Chemin vers le binaire Next.js
-            args: 'start', // Commande pour démarrer Next.js en mode production
+            name: 'artci-signal',
+            script: '/root/ARTCI/artci-signal/node_modules/next/dist/bin/next',
+            args: 'dev -p 5000', // Mode dev avec port personnalisé
             env: {
-                NODE_ENV: 'development', // Variables d'environnement pour développement
-                PORT: 3010, // Port par défaut (modifiable selon tes besoins)
+                NODE_ENV: 'development', // Par défaut
+                PORT: 3000,
             },
             env_production: {
-                NODE_ENV: 'production', // Variables pour production
-                PORT: 4000, // Port personnalisé pour production
+                NODE_ENV: 'production',
+                PORT: 4000,
             },
             env_test: {
-                NODE_ENV: 'test', // Variables pour test
-                PORT: 5000, // Port pour environnement de test
+                NODE_ENV: 'test', // Environnement test
+                PORT: 5000,
             },
-            instances: 1, // Une seule instance (Next.js ne bénéficie pas du mode cluster nativement)
-            exec_mode: 'fork', // Mode fork (Next.js ne nécessite pas de cluster)
-            watch: false, // Désactiver le watch en production pour éviter des redémarrages inutiles
-            autorestart: true, // Redémarrer automatiquement en cas de crash
-            max_memory_restart: '1G', // Redémarrer si la mémoire dépasse 1 Go
-            cwd: './', // Répertoire de travail (racine de ton projet Next.js)
+            instances: 1,
+            exec_mode: 'fork',
+            watch: true, // Activer le watch pour un environnement de test/dev
+            autorestart: true,
+            max_memory_restart: '1G',
+            cwd: '/root/ARTCI/artci-signal',
         },
     ],
 };
