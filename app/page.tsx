c7c30e02949b99e28d253aab446d85688b71fff8
Medi G Fulgence <lycorisblue99@gmail.com>
2025-04-02 05:17:34 +0000

    feat(page): remplacer les balises <a> par des composants <Link> pour une navigation optimisée

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { getFromStorage } from "@/lib/utils/storage";
+import Link from "next/link";
 
 export default function Home() {
   const router = useRouter();
@@ -37,18 +38,18 @@ export default function Home() {
         </p>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-orange-500 hover:bg-orange-600 text-white gap-2 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="/admin/dashboard"
           >
             Tableau de bord
-          </a>
-          <a
+          </Link>
+          <Link
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="/admin/signalements"
           >
             Voir les signalements
-          </a>
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
